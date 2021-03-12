fcd84039788f5bb0657f44a3a58ad182cdecc392
Genor Chiomento <genor.chiomento@gmail.com>
2021-03-11 21:08:50 -0300

    add product list

--- app/src/main/java/br/com/xnrstudio/spotapp/ui/products/fragment/ProductListFragment.kt
+++ app/src/main/java/br/com/xnrstudio/spotapp/ui/products/fragment/ProductListFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.xnrstudio.spotapp.databinding.FragmentProductListBinding
+import br.com.xnrstudio.spotapp.model.ProductResponse
 import br.com.xnrstudio.spotapp.repository.ProductListRepository
 import br.com.xnrstudio.spotapp.repository.api.Resource
 import br.com.xnrstudio.spotapp.repository.api.service.ProductListService
 import br.com.xnrstudio.spotapp.ui.BaseFragment
+import br.com.xnrstudio.spotapp.ui.products.adapter.ProductListAdapter
 import br.com.xnrstudio.spotapp.ui.products.viewmodel.ProductListViewModel
 import br.com.xnrstudio.spotapp.util.handleApiError
 import br.com.xnrstudio.spotapp.util.visible
@@ -18,6 +21,8 @@ import kotlinx.coroutines.runBlocking
 
 class ProductListFragment :
   BaseFragment<ProductListViewModel, FragmentProductListBinding, ProductListRepository>() {
+
+  private lateinit var productAdapter: ProductListAdapter
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
@@ -30,7 +35,9 @@ class ProductListFragment :
       when (product) {
         is Resource.Success -> {
           binding.progressBarList.visible(false)
-          updateList(product.value[0].desMarca)
+          updateList(product.value)
+
+          productAdapter.notifyDataSetChanged()
         }
 
         is Resource.Loading -> {
@@ -42,9 +49,31 @@ class ProductListFragment :
     })
   }
 
-  private fun updateList(productList: String) = with(binding) {
-    tvMarcaProduto.text = productList
+  private fun updateList(productList: ProductResponse) = with(binding) {
+    initRecycler()
+    productAdapter.setProductList(productList)
+    productAdapter.notifyDataSetChanged()
   }
+
+  private fun initRecycler() {
+    binding.rvProductList.apply {
+      layoutManager = LinearLayoutManager(requireContext())
+      productAdapter = ProductListAdapter()
+      adapter = productAdapter
+    }
+  }
+
+//  fun setProducts() {
+//    val item = ArrayList<String>()
+//    item.add("marca 1")
+//    item.add("marca 2")
+//    item.add("marca 3")
+//    item.add("marca 4")
+//    item.add("marca 5")
+//
+//    productAdapter.setProductList(item)
+//    productAdapter.notifyDataSetChanged()
+//  }
 
   override fun getViewModel() = ProductListViewModel::class.java
 
